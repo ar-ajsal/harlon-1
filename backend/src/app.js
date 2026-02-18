5ed4ccfa5a96098acd720ac760409ffafa8ad6b9
Abdu rahman ajsal <ajsala321@gmail.com>
2026-02-19 01:05:00 +0530

    Add new CORS origin for www.harlon.shop

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,7 @@ const corsOptions = {
     origin: [
         process.env.FRONTEND_URL,
         'https://harlon-1.vercel.app',
+        'https://www.harlon.shop/',
         'http://localhost:5173',
         'http://localhost:5174',
         'http://127.0.0.1:5173',
